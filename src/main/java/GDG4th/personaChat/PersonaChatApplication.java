4bc03eed34c9f92bc3c167f1bd3ebbce2bb54557
BaeJunho <bjh4498@knu.ac.kr>
2025-03-04 21:15:10 +0900

    feat ; 초기 설정

---
+++ src/main/java/GDG4th/personaChat/PersonaChatApplication.java
@@ -0,0 +1,13 @@
+package GDG4th.personaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDG4th/personaChat/PersonaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package GDG4th.personaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
