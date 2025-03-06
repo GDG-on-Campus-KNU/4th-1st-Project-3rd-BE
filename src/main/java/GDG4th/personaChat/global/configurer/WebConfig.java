2db03f9fadb365dadf35ae621e91ed35a6a4cc08
BaeJunho <bjh4498@knu.ac.kr>
2025-03-06 22:21:50 +0900

    [PERSONA-5] chore: cors config 설정
    아직 communication은 없지만 미리 설정

---
+++ src/main/java/GDG4th/personaChat/global/configurer/WebConfig.java
@@ -0,0 +1,15 @@
+package GDG4th.personaChat.global.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS");
+    }
+}
