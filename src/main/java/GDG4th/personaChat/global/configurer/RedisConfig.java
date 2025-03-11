95a0badb2f1543bba69b411dc6f1afb00fd69ef5
BaeJunho <bjh4498@knu.ac.kr>
2025-03-11 21:47:02 +0900

    [PERSONA-10] redis config 변경 및 jpa dialect 설정
    port, host 커스텀을 위해 autoConfiguration 활용 & DB 사용 설정 명시

--- src/main/java/GDG4th/personaChat/global/configurer/RedisConfig.java
+++ src/main/java/GDG4th/personaChat/global/configurer/RedisConfig.java
@@ -2,21 +2,16 @@ package GDG4th.personaChat.global.configurer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Bean
-    LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
 
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
