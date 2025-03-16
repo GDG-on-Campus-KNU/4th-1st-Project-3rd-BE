9b9fcb902e5d02adb4f94fefccb639a3e308af18
BaeJunH0 <bjh14757@naver.com>
2025-03-16 22:19:59 +0900

    [PERSONA-12] 캐시 엔티티 이름 변경

---
+++ src/main/java/GDG4th/personaChat/chat/domain/ChatCache.java
@@ -0,0 +1,37 @@
+package GDG4th.personaChat.chat.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "chat_log")
+public class ChatCache implements Serializable {
+    @Id
+    private String userId;
+
+    private String userMBTI;
+
+    private List<Message> messages;
+
+    public ChatCache(Long userId, String userMBTI, List<Message> messages) {
+        this.userId = userId.toString();
+        this.userMBTI = userMBTI;
+        this.messages = messages;
+    }
+
+    public void clearCache() {
+        this.messages.clear();
+    }
+
+    public void addCache(Message message) {
+        this.messages.add(message);
+    }
+}
