c2149535a031514854dcdf82e7f6afffe60f3941
BaeJunH0 <bjh14757@naver.com>
2025-03-24 17:09:24 +0900

    PERSONA-12 test: 테스트 코드 기반 작성

---
+++ src/test/java/GDG4th/personaChat/chat/application/ChatServiceTest.java
@@ -0,0 +1,15 @@
+package GDG4th.personaChat.chat.application;
+
+import org.junit.jupiter.api.Test;
+
+
+class ChatServiceTest {
+    @Test
+    void responseMessage() {
+
+    }
+
+    @Test
+    void receiveUserMessage() {
+    }
+}
