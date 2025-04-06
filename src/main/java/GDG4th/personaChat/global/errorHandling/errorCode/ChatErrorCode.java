6d3206d15c63da590bd70293cb13dc9dde9f3168
BaeJunH0 <bjh14757@naver.com>
2025-04-06 22:55:39 +0900

    PERSONA-17 feat: 기능 구현 과정에서 필요한 에러코드 정의

--- src/main/java/GDG4th/personaChat/global/errorHandling/errorCode/ChatErrorCode.java
+++ src/main/java/GDG4th/personaChat/global/errorHandling/errorCode/ChatErrorCode.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ChatErrorCode implements ErrorCode{
-    LOGICAL_ERROR(HttpStatus.BAD_REQUEST, "C001", "Chat log must be even");
+    LOGICAL_ERROR(HttpStatus.BAD_REQUEST, "C001", "Chat log must be even"),
+    ALREADY_OPEN(HttpStatus.NO_CONTENT, "C002", "It's already opened"),
+    ALREADY_CLOSED(HttpStatus.NO_CONTENT, "C003", "It's already opened"),
+    IS_NOT_OPEN_CHAT(HttpStatus.BAD_REQUEST, "C004", "that mbti chat is not opened yet");
 
     private HttpStatus httpStatus;
     private String code;
