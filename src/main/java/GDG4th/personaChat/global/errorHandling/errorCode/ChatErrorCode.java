e1a2911da30a83c350e585cc6d352d6d05b63b58
BaeJunH0 <bjh14757@naver.com>
2025-04-09 12:08:14 +0900

    PERSONA-17 fix: Error 정책 변경
    채팅방이 열려있을 때 열기, 닫혀있을 때 닫기 동작에 대해 BAD_REQUEST 적용 ( 400 )

--- src/main/java/GDG4th/personaChat/global/errorHandling/errorCode/ChatErrorCode.java
+++ src/main/java/GDG4th/personaChat/global/errorHandling/errorCode/ChatErrorCode.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ChatErrorCode implements ErrorCode{
     LOGICAL_ERROR(HttpStatus.BAD_REQUEST, "C001", "Chat log must be even"),
-    ALREADY_OPEN(HttpStatus.NO_CONTENT, "C002", "It's already opened"),
-    ALREADY_CLOSED(HttpStatus.NO_CONTENT, "C003", "It's already opened"),
+    ALREADY_OPEN(HttpStatus.BAD_REQUEST, "C002", "It's already opened"),
+    ALREADY_CLOSED(HttpStatus.BAD_REQUEST, "C003", "It's already opened"),
     IS_NOT_OPEN_CHAT(HttpStatus.BAD_REQUEST, "C004", "that mbti chat is not opened yet");
 
     private HttpStatus httpStatus;
