5a1693318d8508a5d6457803c5166fafa8d40374
BaeJunH0 <bjh14757@naver.com>
2025-05-07 17:16:06 +0900

    PERSONA-22 fix : NPE 해결

--- src/main/java/GDG4th/personaChat/chat/application/dto/RecentChatLog.java
+++ src/main/java/GDG4th/personaChat/chat/application/dto/RecentChatLog.java
@@ -6,7 +6,7 @@ public record RecentChatLog(
         boolean isViewed
 ) {
     public static RecentChatLog of(String mbti, LastChatInfo lastChatInfo, boolean isViewed) {
-        if(lastChatInfo.chatLog().isEmpty()) {
+        if(lastChatInfo.chatLog() == null) {
             return new RecentChatLog(mbti, lastChatInfo, true);
         }
         return new RecentChatLog(mbti, lastChatInfo, isViewed);
