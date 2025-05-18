4028f4f340927c2482c37503fb50e052ab38d6b6
BaeJunH0 <bjh14757@naver.com>
2025-05-19 00:30:48 +0900

    PERSONA-19 feat: 채팅 정보 반환 구현

--- src/main/java/GDG4th/personaChat/chat/persistent/ChatRepository.java
+++ src/main/java/GDG4th/personaChat/chat/persistent/ChatRepository.java
@@ -2,6 +2,8 @@ package GDG4th.personaChat.chat.persistent;
 
 import GDG4th.personaChat.chat.domain.Chat;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Date;
@@ -9,6 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ChatRepository extends MongoRepository<Chat, ObjectId> {
+    Page<Chat> findChatsByUserId(String userId, Pageable pageable);
+
     List<Chat> findByUserIdAndMbtiAndTimestampAfter(
             String userId, String mbti, Date timestamp
     );
